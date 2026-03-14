ebd3dbc4fcdf4cf79b7b1f5794ec3ffddab5ee6b
Gayathri-Sangeetha <gayathrisangeetha28@gmail.com>
2026-03-14 21:41:37 +0530

    Created session model

---
+++ backend/models/Session.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const sessionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    questions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Question",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Session", sessionSchema);
